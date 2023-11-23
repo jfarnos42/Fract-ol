58751337df97178a4450cd11c55a4bfa1113f9dd
Joaquin Farnos Belles <jfarnos-@c1r7s6.42madrid.com>
2023-11-23 23:17:46 +0100

    fractol UPDATE

---
+++ Makefile
@@ -0,0 +1,32 @@
+NAME = fractol
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+RM = rm -rf
+
+SRC = fractol.c
+
+OBJ_DIR = obj
+OBJ	= $(addprefix ${OBJ_DIR}/, ${SRC:%.c=%.o})
+
+all: $(NAME)
+
+${NAME}: ${OBJ}
+	@${MAKE} -C ./Libft
+	@${MAKE} -C ./ft_printf
+	@${CC} ${CFLAGS} ${OBJ} ./Libft/libft.a ./ft_printf/libftprintf.a -o ${NAME}
+
+${OBJ_DIR}/%.o:./%.c
+	@mkdir -p ${OBJ_DIR}
+	gcc ${FLAGS} -c $< -o $@
+
+clean:
+	@${MAKE} -C ./Libft fclean
+	@${MAKE} -C ./ft_printf fclean
+	@$(RM) $(OBJ_DIR)
+
+fclean: clean
+	@$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ fractol.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfarnos- <jfarnos-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 22:03:21 by jfarnos-          #+#    #+#             */
+/*   Updated: 2023/11/23 23:15:41 by jfarnos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <math.h>
+# include <mlx.h>
+# include "Libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+void	function(void);
+
+#endif
